ed86b514f6d0e1e42e426c6832d0e8d739e27f3f
PRAAVUS <73282565+PRAAVUS@users.noreply.github.com>
2020-11-06 20:35:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChangingConsoleColors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
